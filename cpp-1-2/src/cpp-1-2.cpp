0301c75614d8abd86db423a4cca662878ae3b29d
ddt-sugiura <ddt.jnsk.sugiura@gmail.com>
2021-11-10 21:28:46 +0900

    初回コミット

---
+++ cpp-1-2/src/cpp-1-2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cpp-1-2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+//using namespace std;
+
+int main() {
+	std::cout << "初めてのC++プログラム\n";
+	std::cout << "画面に出力しています\n";
+}

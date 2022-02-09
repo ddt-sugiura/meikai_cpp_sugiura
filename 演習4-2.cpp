323713207f580ec49e223493249c7c23410bf38e
ddt-sugiura <94049636+ddt-sugiura@users.noreply.github.com>
2022-02-09 21:30:16 +0900

    Add files via upload
    演習4-2を修正しました。確認のほどよろしくお願いします。

--- 演習4-2.cpp
+++ 演習4-2.cpp
@@ -12,57 +12,75 @@ int main() {
 	cout << "int型8進行\n";
 	//処理が終わらないため，最後の20個のみを表示させる
 	int StartIntNo = INT_MAX - 20;
-	//for (int Int_go8 = 0; ; Int_go8++) {　本来のfor分
-	for (int Int_go8 = StartIntNo; ; Int_go8++) {
+	//最大値になるまでループ
+	//for (int Int_go8 = 0; Int_go8 < INT_MAX ; Int_go8++) {　本来のfor分
+	for (int Int_go8 = StartIntNo;Int_go8 < INT_MAX; Int_go8++) {
 		//8進行に変換する
 		cout << oct << Int_go8;
-		//最大値になったら処理を終了する
-		if(Int_go8 == INT_MAX) break;
 		//ループに戻る前にコンマを出力
 		else cout << ",";
 	}
-	cout << "\n";
+	//最後に最大値を出力して改行
+	cout << oct << INT_MAX <<",\n";
+
 	//intの10進行を出力する列
 	cout << "int型10進行\n";
-	//for (int Int_go10 = 0; ; Int_go10++) {
-	for (int Int_go10 = StartIntNo; ; Int_go10++) {
+	//最大値になるまでループ
+	//for (int Int_go10 = 0; Int_go10 < INT_MAX ; Int_go10++) {　本来のfor分
+	for (int Int_go10 = StartIntNo; Int_go10 < INT_MAX; Int_go10++) {
 		//10進行に変換する
 		cout << dec << Int_go10;
-		if (Int_go10 == INT_MAX) break;
+		//ループに戻る前にコンマを出力
 		else cout << ",";
 	}
-	cout << "\n";
+	//最後に最大値を出力して改行
+	cout << oct << INT_MAX << ",\n";
+
 	//intの16進行を出力する列
 	cout << "int型16進行\n";
-	//for (int Int_go16 = 0; ; Int_go16++) {
-	for (int Int_go16 = StartIntNo; ; Int_go16++) {
+	//最大値になるまでループ
+	//for (int Int_go16 = 0; Int_go16 < INT_MAX ; Int_go16++) {
+	for (int Int_go16 = StartIntNo; Int_go16 < INT_MAX ; Int_go16++) {
 		//16進行に変換する
 		cout << hex <<Int_go16;
+		//最大値になったら処理を終了する
 		if (Int_go16 == INT_MAX) break;
+		//ループに戻る前にコンマを出力
 		else cout << ",";
 	}
-	cout << "\n";
+	//最後に最大値を出力して改行
+	cout << oct << INT_MAX << ",\n";
+
 	//unsingintの8進行を出力する列
 	cout << "unsigned型8進行\n";
+	//最大値になるまでループ
+	//for (int UnsignInt_go8 = 0; UnsignInt_go8 < UINT_MAX ; UnsignInt_go8++) {　本来のfor分
 	for (int UnsignInt_go8 = 20; UnsignInt_go8 >= 0 ; UnsignInt_go8--) {
 		//最大値からカウント関数を引いた値を8進行に変換する
 		cout << oct << unsigned(UINT_MAX - UnsignInt_go8);
+		//ループに戻る前にコンマを出力
 		cout << ",";
 	}
 	cout << "\n";
 	///unsingintの10進行を出力する列
 	cout << "unsigned型10進行\n";
+	//最大値になるまでループ
+	//for (int UnsignInt_go10 = 0; UnsignInt_go10 < UINT_MAX ; UnsignInt_go10++) {　本来のfor分
 	for (int UnsignInt_go10 = 20; UnsignInt_go10 >= 0; UnsignInt_go10--) {
 		//最大値からカウント関数を引いた値を10進行に変換する
 		cout << dec << unsigned(UINT_MAX - UnsignInt_go10);
+		//ループに戻る前にコンマを出力
 		cout << ",";
 	}
 	cout << "\n";
 	///unsingintの16進行を出力する列
 	cout << "unsigned型16進行\n";
+	//最大値になるまでループ
+	//for (int UnsignInt_go16 = 0; UnsignInt_go16 < UINT_MAX ; UnsignInt_go16++) {　本来のfor分
 	for (int UnsignInt_go16 = 20; UnsignInt_go16 >= 0; UnsignInt_go16--) {
 		//最大値からカウント関数を引いた値を16進行に変換する
 		cout << hex << unsigned(UINT_MAX - UnsignInt_go16);
+		//ループに戻る前にコンマを出力
 		cout << ",";
 	}
 }

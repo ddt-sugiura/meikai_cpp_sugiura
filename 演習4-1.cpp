6b058930d370efd18aab62532e479c2f3ae81d6c
ddt-sugiura <94049636+ddt-sugiura@users.noreply.github.com>
2022-03-16 20:39:16 +0900

    Add files via upload
    ご指摘頂いたようにコメントを追加いたしました．
    確認のほどよろしくお願いします。

--- 演習4-1.cpp
+++ 演習4-1.cpp
@@ -7,22 +7,30 @@ using namespace std;
  すべての文字について，数字/英大文字/英小文字/記号文字などの文字列がわかる
  一覧表を出力するプログラムを作成せよ*/
 int main() {
+	//1行目にタイトルを出力して改行
 	cout << "一覧表\n";
+	//フォーマット調整のため空白を出力
 	cout << "   ";
-	//横の項目名を作る
+	//2行目に横の項目名を作る
 	for (int horizontal_name = 0; horizontal_name < 16; horizontal_name++) {
+		//16進数で出力
 		cout << " " << hex << horizontal_name;
 	}
+	//項目名の下に仕切りをつくるため3行目に改行
 	cout << "\n";
+	//フォーマット調整のため空白を出力
 	cout << "   ";
 	//横線を引く
 	for (int horizontal_no = 0; horizontal_no < 16; horizontal_no++) {
 		cout << "--";
 	}
+	//4行目へ改行
 	cout << "\n";
 	char onlychar;
 	for (int vartical = 0; vartical <16 ; vartical++) {
-		cout << hex << vartical << " |"; //縦線を引く
+		//行の項目名を出力して縦線を引く
+		cout << hex << vartical << " |";
+		//列の数だけ繰り返し出力して改行するループ
 		for (int horizontal_no = 0; horizontal_no <16; horizontal_no++) {
 			onlychar = char(vartical *16 + horizontal_no);
 			switch (onlychar)

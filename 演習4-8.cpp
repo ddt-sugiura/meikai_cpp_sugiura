7f3d9ffa12973e2c8d76a12614fa20e3ba68a2d7
ddt-sugiura <94049636+ddt-sugiura@users.noreply.github.com>
2022-04-06 20:13:40 +0900

    演習4-8
    演習4-8を修正しました。確認のほどよろしくお願いします。

--- 演習4-8.cpp
+++ 演習4-8.cpp
@@ -12,12 +12,12 @@ int main() {
 	int TypeOf_I = 1;
 	//double型の変数を宣言
 	double TypeOf_D = 1;
-	// double + double の 各演算子による出力結果
+	// int + int の 各演算子による出力結果 int同士の計算なので,int型で出力される
 	std::cout << "int + int : sizeof演算子 " << sizeof(TypeOf_I + TypeOf_I) << " typeid演算子 " << typeid(TypeOf_I + TypeOf_I).name() << "\n";
-	// int+int の 各演算子による出力結果
+	// double + double の 各演算子による出力結果 double同士の計算なので，double型で出力される
 	std::cout << "double + double : sizeof演算子 " << sizeof(TypeOf_D + TypeOf_D) << " typeid演算子 " << typeid(TypeOf_D + TypeOf_D).name() << "\n";
-	// int + double の 各演算子による出力結果
+	// int + double の 各演算子による出力結果　double型が含まれているため，計算を成立されるためbouble型で出力される
 	std::cout << "int + double : sizeof演算子 " << sizeof(TypeOf_I + TypeOf_D) << " typeid演算子 " << typeid(TypeOf_I + TypeOf_D).name() << "\n";
-	// int+int の 各演算子による出力結果
+	// int+int の 各演算子による出力結果　上記と同様
 	std::cout << "double + int : sizeof演算子 " << sizeof(TypeOf_D + TypeOf_I) << " typeid演算子 " << typeid(TypeOf_D + TypeOf_I).name() << "\n";
 }

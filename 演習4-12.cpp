75c25c8bd9fd401edef3faa93514839731416f66
ddt-sugiura <94049636+ddt-sugiura@users.noreply.github.com>
2022-04-13 21:16:34 +0900

    Add files via upload
    演習4-12の修正

--- 演習4-12.cpp
+++ 演習4-12.cpp
@@ -9,9 +9,9 @@ using namespace std;
   例題同様キーボードから値を読み込み表示すること*/
 int main() {
 	//順にダイヤ，ハート，クラブ，スペードを列挙
-	enum EnumerationTramp { diamond, heart, club, spade, Invalid };
+	enum EnumerationTramp { e_DIAMOND, e_HEART, e_CLUB, e_SPADE, e_INVALID };
 	//列挙子を選択するための変数
-	int EnumeratorType;
+	int EnumeratorType = 0;
 	//ループ実行
 	do{
 		//選択肢を表示
@@ -21,34 +21,34 @@ int main() {
 		cin >> EnumeratorType;
 	}
 	// 0以上4以下でなければもう一度選択させる
-	while (EnumeratorType < diamond || EnumeratorType > Invalid);
+	while (EnumeratorType < e_DIAMOND || EnumeratorType > e_INVALID);
 	// Invalid以外を選択した場合に出力処理をする
-	if (EnumeratorType != Invalid) {
+	if (EnumeratorType != e_INVALID) {
 		//列挙子のいずれかで変数を宣言
 		EnumerationTramp SelectedMark = static_cast<EnumerationTramp>(EnumeratorType);
 		//変数によって出力内容を変更する
 		switch (SelectedMark)
 		{
 			//Invalid以外の選択肢の処理を実装
-		case diamond:
+		case e_DIAMOND:
 			//選択したマークを示す
 			cout << "ダイヤを選択";
 			//switchを抜ける
 			break;
 
-		case heart:
+		case e_HEART:
 			//選択したマークを示す
 			cout << "ハートを選択";
 			//switchを抜ける
 			break;
 
-		case club:
+		case e_CLUB:
 			//選択したマークを示す
 			cout << "クラブを選択";
 			//switchを抜ける
 			break;
 
-		case spade:
+		case e_SPADE:
 			//選択したマークを示す
 			cout << "スペードを選択";
 			//switchを抜ける

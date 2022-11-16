5e7f299664cdace19a647cd80a7424ba22765353
ddt-sugiura <94049636+ddt-sugiura@users.noreply.github.com>
2022-11-16 20:37:29 +0900

    Add files via upload
    演習5-12を修正しましたので確認のほどよろしくお願いします。

--- 演習5-12.cpp
+++ 演習5-12.cpp
@@ -14,15 +14,15 @@ int main() {
 	//coutで式を出力していく
 	cout << " 3次元配列の要素数を求める, \n\n";
 	//一次元目の要素数
-	int firstElement = sizeof(threeDimension) / sizeof(threeDimension[0]);
+	int firstElementSize = sizeof(threeDimension) / sizeof(threeDimension[0]);
 	//二次元目の要素数
-	int secondElement = sizeof(threeDimension[0]) / sizeof(threeDimension[0][0]);
+	int secondElementSize = sizeof(threeDimension[0]) / sizeof(threeDimension[0][0]);
 	//三次元目の要素数
-	int thirdErement = sizeof(threeDimension[0][0]) / sizeof(threeDimension[0][0][0]);
+	int thirdErementSize = sizeof(threeDimension[0][0]) / sizeof(threeDimension[0][0][0]);
 	//全要素数
-	int allElement = sizeof(threeDimension) / sizeof(threeDimension[0][0][0]);
+	int allElementSize = sizeof(threeDimension) / sizeof(threeDimension[0][0][0]);
 	//配列の各次元の要素数をわかりやすく出力
-	cout << " 3次元配列[" << firstElement << "][" << secondElement << "][" << thirdErement << "]の要素数は, \n\n";
+	cout << " 3次元配列[" << firstElementSize << "][" << secondElementSize << "][" << thirdErementSize << "]の要素数は, \n\n";
 	//求める式がわかるように出力
-	cout << " " << firstElement << " × " << secondElement << " × " << thirdErement << " = " << allElement;
+	cout << " " << firstElementSize << " × " << secondElementSize << " × " << thirdErementSize << " = " << allElementSize;
 }
